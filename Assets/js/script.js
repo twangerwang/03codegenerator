93cd9ea977da1aa950eeeb12428289eb7ea12d87
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-08-16 02:11:26 -0500

    initial files

---
+++ Assets/js/script.js
@@ -0,0 +1,62 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function generatePassword() {
+ 
+  // Takes in a password length and check to see if it matches the length criteria
+  var pwLength = window.prompt("Length of password, 8 - 128 characters.");
+    while ((pwLength < 8) || (pwLength > 128)) {
+      pwLength = window.prompt("Please enter a length between 8 - 128 characters for the password again.");
+    }
+
+// Sets the 4 individual types of characters to be used in the password
+  let lowerCasePool = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let upperCasePool = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+  let numericPool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let specCharPool = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '}', '~'];
+
+// initializes the final pool which the password will pick from
+  var totalPool = [];
+  
+// next 4 vars selects which subpool to add to the final pool
+  var lowerCase = window.confirm("Click OK to include lowercase letters");
+  if (lowerCase) {
+    totalPool = totalPool.concat(lowerCasePool);
+  } 
+  
+  var upperCase = window.confirm("Click OK to include UPPERCASE letters");
+  if (upperCase) {
+    totalPool = totalPool.concat(upperCasePool);
+  }
+  
+  var numeric = window.confirm("Click OK to include numerics");
+  if (numeric) {
+    totalPool = totalPool.concat(numericPool);
+  } 
+  
+  var specChar = window.confirm("Click OK to include special characters");
+  if (specChar) {
+    totalPool = totalPool.concat(specCharPool);
+  } 
+
+  // ititializes the password string then runs a while loop for the length of the password
+  var pw = [];
+  while (pwLength > 0) {
+    pw = pw + totalPool[Math.floor(Math.random() * totalPool.length)];
+    pwLength--;
+  }
+  
+ return pw;
+ 
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+ passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # 03codegenerator
+Create a Password Generator that takes a length input and go through a series of prompt to determine which sets of characters to include in the password.
+
+## Descrciption
+*Prompt for length of password (will prompt again if length is outside of asked range)
+*Prompt for inclusion of types of characters (lower case, upper case, numerics, or special characters)
+*Password is generated and printed in the password box
+
+## Mock-up
+The image below shows the webpage created
+
+![alt text](assets/images/PW-result.png)
+
+## Location
+https://twangerwang.github.io/02codegenerator/
+
+## Credits
+Original code provided by University of Texas Coding Bootcamp
+
+## License
+MIT License
+
+Copyright (c) 2022 Teddy Wang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
